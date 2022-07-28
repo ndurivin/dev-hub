8c29b7f938ec845def76c4b6667875f915a476c6
Vinn Nduri <ndurivo@gmail.com>
2022-07-28 22:48:57 +0300

    Add the DevCard component to the Home component and add its props

--- src/components/DevCard.js
+++ src/components/DevCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function DevCard({devName, devThumbnail, devId}) {
   return (
@@ -6,13 +7,13 @@ function DevCard({devName, devThumbnail, devId}) {
         <div className="row row-cols-1 row-cols-md-3 g-4">
             <div className="col">
             <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={devThumbnail} className="card-img-top" alt={devName} />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">{devName}</h5>
+                    <Link to={'/dev/' + {devId}} className="btn btn-info">View Details</Link>
                 </div>
             </div>
-        </div>
+            </div>
         </div>
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import DevCard from './DevCard';
 
 const Home = () => {
   return (
     <div>
-      
+        <DevCard devName="" devId="" devThumbnail="" />
     </div>
   )
 }
