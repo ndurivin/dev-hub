e5371d0d3ec54dbbb9a48df5961af1d6a475521c
Vinn Nduri <ndurivo@gmail.com>
2022-07-28 20:58:15 +0300

    Add different components to App & create routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Devs from "./components/Devs";
+import Admin from "./components/Admin";
+
 
 function App() {
   return (
@@ -8,6 +12,11 @@ function App() {
         <div>
           <Navbar />
         </div>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/devs" element={<Devs />}></Route>
+          <Route exact path="/admin" element={<Admin />}></Route>
+        </Routes>
       </Router>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             <div className="container-fluid">
                 <Link to="/" className="navbar-brand">React-Devs-Hub</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -25,7 +25,7 @@ const Navbar = () => {
                 </form>
             </div>
         </div>
-    </nav>
+        </nav>
     </div>
   )
 }
