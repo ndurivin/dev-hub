edaf1ccfc91af6c30e7f984772730688191e532b
Vinn Nduri <ndurivo@gmail.com>
2022-07-28 20:43:33 +0300

    Create links to home and devs on the navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+      </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-info">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">React-Devs-Hub</a>
+                <Link to="/" className="navbar-brand">React-Devs-Hub</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <Link to="/" className="nav-link active" aria-current="page">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled">Disabled</a>
+                        <Link to="/devs" className="nav-link active" aria-current>Devs</Link>
                     </li>
                     </ul>
                 <form className="d-flex" role="search">
