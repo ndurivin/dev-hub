959fff0c4bc484afda9801b35568b802f24f9742
Vinn Nduri <ndurivo@gmail.com>
2022-07-28 21:35:59 +0300

    Create the dev Card in the DevCard component

--- src/components/DevCard.js
+++ src/components/DevCard.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function DevCard({}) {
+function DevCard({devName, devThumbnail, devId}) {
   return (
     <div>
-      
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+            <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
   )
 }
