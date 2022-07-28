f1504ecb486f91fa5ab1c756927d710c33a4d6ca
Vinn Nduri <ndurivo@gmail.com>
2022-07-28 23:33:31 +0300

    Added the API url link to home page

--- src/components/DevCard.js
+++ src/components/DevCard.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 function DevCard({devName, devThumbnail, devId}) {
   return (
-    <div>
+    <div className='container'>
         <div className="row row-cols-1 row-cols-md-3 g-4">
             <div className="col">
-            <div className="card">
+            <div className="card align-items-center">
                 <img src={devThumbnail} className="card-img-top" alt={devName} />
-                <div className="card-body">
-                    <h5 className="card-title">{devName}</h5>
+                <div className="card-body align-items-center">
+                    <h4 className="card-title text-center">{devName}</h4>
                     <Link to={'/dev/' + {devId}} className="btn btn-info">View Details</Link>
                 </div>
             </div>
@@ -20,3 +20,5 @@ function DevCard({devName, devThumbnail, devId}) {
 }
 
 export default DevCard;
+
+// style={{height: '15vw'}}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DevCard from './DevCard';
 
+const api_url = "https://quiet-hamlet-90428.herokuapp.com/devs";
+
 const Home = () => {
+
+
+const [devs, setDevs] = useState([])
+
+const getDeveloper = () => {
+    fetch(api_url)
+    .then((response) => response.json())
+    .then((data) => {
+        devs = data.devs
+    })
+}
+
   return (
-    <div>
-        <DevCard devName="" devId="" devThumbnail="" />
+    <div className='container'>
+        <br/>
+        <DevCard devName="Wcats" devId="2" devThumbnail="https://avatars.githubusercontent.com/u/8957173?v=4" />
     </div>
   )
 }
