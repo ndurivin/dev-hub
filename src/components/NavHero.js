ca8e86b69a512d2add0d70dcf8773e8798a40f62
Vinn Nduri <ndurivo@gmail.com>
2022-07-29 08:44:56 +0300

    Fix the Navbar Hero text on image error

--- src/components/NavHero.js
+++ src/components/NavHero.js
@@ -8,7 +8,6 @@ const NavHero = () => {
         <div className="card text-bg-light">
             <img src="https://www-cdn.bigcommerce.com/assets/article-blog-headers-generic-seo-store-1.png" className="card-img" alt="..."/>
             <div className="card-img-overlay">
-                <h5 className="card-title"></h5>
                 <p className="card-text"></p>
                 <p className="card-text"></p>
             </div>
