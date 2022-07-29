6ad874082dfe72f584c62b2e0f4330aea4123e8e
Vinn Nduri <ndurivo@gmail.com>
2022-07-29 06:38:14 +0300

    Fix details link to point to  each dev details

--- src/components/DevCard.js
+++ src/components/DevCard.js
@@ -3,19 +3,15 @@ import { Link } from 'react-router-dom';
 
 function DevCard({devName, devThumbnail, devId}) {
   return (
-    <div className='container'>
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-            <div className="col">
-            <div className="card h-100 shadow align-items-center">
-                <img src={devThumbnail} className="card-img-top" alt={devName} />
-                <div className="card-body align-items-center">
+           <div className='row-cols-3'>
+             <div className="card h-100 shadow align-items-center py-3" style={{width: 18 + "rem"}}>
+                <img src={devThumbnail} className="card-img-top img-fluid" alt={devName} />
+                <div className="card-body">
                     <h4 className="card-title text-center">{devName}</h4>
-                    <Link to={'/dev/' + {devId}} className="btn btn-info">View Details</Link>
+                    <Link to={'/devs/' +devId} className="btn btn-info">View Details</Link>
                 </div>
             </div>
-            </div>
-        </div>
-    </div>
+           </div>
   )
 }
 
