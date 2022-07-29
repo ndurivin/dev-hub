65e46bac7d02c4f88dacb72a32fb344351b796dd
Vinn Nduri <ndurivo@gmail.com>
2022-07-29 07:20:33 +0300

    Add useState for ech of profile details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Profile = () => {
+
+
+    const [name, setName] = useState("")
+    const [image, setImage] = useState("")
+    const [tech, setTech] = useState("")
+    const [experience, setExperience] = useState("")
+    const [followers, setFollowers] = useState("")
+    const [following, setFollowing] = useState("")
+
+
+
   return (
     <div>
         <center>
@@ -12,9 +23,10 @@ const Profile = () => {
                     <div className="col-md-8">
                     <div className="card-body">
                         <h4 className="card-title">Card title</h4>
-                        <h5 className="card-text">This is a wider card.</h5>
-                        <h5 className="card-text"><small className="text-muted">Last updated 3 mins ago</small></h5>
-                        <h5 className="card-text"><small className="text-muted">Last updated 3 mins ago</small></h5>
+                        <h5 className="card-text">Technology Stack</h5>
+                        <h5 className="card-text">Years of Experieince</h5>
+                        <h6 className="card-text"><small className="text-muted">Followers</small></h6>
+                        <h6 className="card-text"><small className="text-muted">Following</small></h6>
                     </div>
                     </div>
                 </div>
