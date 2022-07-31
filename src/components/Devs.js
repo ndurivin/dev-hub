78c0f3e3983e44ce19bda52c43c27afba4334b13
Vinn Nduri <ndurivo@gmail.com>
2022-07-31 07:46:33 +0300

    Move data.json to main folder

--- src/components/Devs.js
+++ src/components/Devs.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import DevCard from './DevCard';
 import Search from './Search';
+import DevData from './data.'
 
 const Devs = () => {
   return (
     <div className="container">
       <br />
-      <Search />
+      <Search placeholder="Search for dev by tech stack..."/>
       <br/>
         <h2 className="text-center py-4">Featured Developers</h2>
       <DevCard />
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import data from '../components/data/data'
 
-const Search = () => {
-data.map((value, key) => {
-    return <div>
-        {value.name}
-    </div>
-})
+const Search = ({ placeholder, data }) => {
+
 
   return (
     <div>
         
         <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" 
-                placeholder="Search" aria-label="Search"/>
+               placeholder ={placeholder}aria-label="Search"/>
             <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
 
